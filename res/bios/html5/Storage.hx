72c88f266b23d80028a2e25d927c140f71821bb0
Michael Miriti <m.s.miriti@gmail.com>
2022-05-08 21:43:34 +0300

    add `override` to `save`/`restore` methods of `Storage`

--- res/bios/html5/Storage.hx
+++ res/bios/html5/Storage.hx
@@ -6,11 +6,11 @@ import js.Browser;
 class Storage extends res.storage.Storage {
 	static final LS_KEY:String = 'res_storage';
 
-	public function save() {
+	override public function save() {
 		Browser.getLocalStorage().setItem(LS_KEY, Json.stringify(data));
 	}
 
-	public function restore() {
+	override public function restore() {
 		final s_data = Browser.getLocalStorage().getItem(LS_KEY);
 		if (s_data != null)
 			data = Json.parse(s_data);
